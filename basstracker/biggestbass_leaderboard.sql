f5fd5c5ede13e65610d564458aafbcfdb65b0818
JVue <16140405+JVue@users.noreply.github.com>
2020-05-25 12:51:35 -0500

    Update biggestbass_leaderboard.sql

--- basstracker/biggestbass_leaderboard.sql
+++ basstracker/biggestbass_leaderboard.sql
@@ -1,39 +1,76 @@
-;with cteMaxLargeMouth as (
-  select angler, max(weight_oz) as MaxLargeMouthWeight
-  from basstracker
-  where bass_type = 'largemouth'
-  group by angler
-), cteMaxSmallMouth as (
-  select angler, max(weight_oz) as MaxSmallMouthWeight
-  from basstracker
-  where bass_type = 'smallmouth'
-  group by angler
-), cteCombinedSmallMouthAndLargeMouth as (
-  select angler, MaxLargeMouthWeight as Weight
-  from cteMaxLargeMouth
-  union all
-  select angler, MaxSmallMouthWeight
-  from cteMaxSmallMouth
-), cteTotalWeight as (
-  select angler, sum(Weight) as TotalWeightOfAnglersMaxLargeAndMaxSmallMouth
-  from cteCombinedSmallMouthAndLargeMouth
-  group by angler
-), cteMaxWeight as (
-  select max(TotalWeightOfAnglersMaxLargeAndMaxSmallMouth) as MaxTotalWeight
-  from cteTotalWeight
-), cteOuncesBehind as (
- select angler, MaxTotalWeight - TotalWeightOfAnglersMaxLargeAndMaxSmallMouth as OuncesBehind
- from cteTotalWeight tw 
- cross join cteMaxWeight mw  
- )
-select 
-  a.angler,
-  concat(floor(l.MaxLargeMouthWeight/16), '-', mod(l.MaxLargeMouthWeight,16)) as largemouth_weight,
-  concat(floor(s.MaxSmallMouthWeight/16), '-', mod(s.MaxSmallMouthWeight,16)) as smallmouth_weight,
-  concat(floor(w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth/16), '-', mod(w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth,16)) as total_weight,
-  concat(floor(b.OuncesBehind/16), '-', mod(b.OuncesBehind,16)) as behind
-from basstracker_anglers a
-left join cteMaxLargeMouth l on l.angler = a.angler
-left join cteMaxSmallMouth s on s.angler = a.angler
-left join cteTotalWeight w on w.angler = a.angler
-left join cteOuncesBehind b on b.angler = a.angler
+WITH cteMaxLargeMouth AS (
+  SELECT
+    angler,
+    max(weight_oz) AS MaxLargeMouthWeight
+  FROM basstracker
+  WHERE
+    bass_type = 'largemouth'
+    and date like '%/$year'
+  GROUP BY angler
+), cteMaxSmallMouth AS (
+  SELECT
+    angler,
+    max(weight_oz) AS MaxSmallMouthWeight
+  FROM basstracker
+  WHERE
+    bass_type = 'smallmouth'
+    and date like '%/$year'
+  GROUP BY angler
+), cteCombinedSmallMouthAndLargeMouth AS (
+  SELECT
+    angler,
+    MaxLargeMouthWeight AS Weight
+  FROM cteMaxLargeMouth
+  UNION ALL
+  SELECT
+    angler,
+    MaxSmallMouthWeight
+  FROM cteMaxSmallMouth
+), cteTotalWeight AS (
+  SELECT
+    angler,
+    sum(Weight) AS TotalWeightOfAnglersMaxLargeAndMaxSmallMouth
+  FROM cteCombinedSmallMouthAndLargeMouth
+  GROUP BY angler
+), cteMaxWeight AS (
+  SELECT
+    max(TotalWeightOfAnglersMaxLargeAndMaxSmallMouth) as MaxTotalWeight
+  FROM cteTotalWeight
+), cteOuncesBehind AS (
+  SELECT
+    angler,
+    MaxTotalWeight - TotalWeightOfAnglersMaxLargeAndMaxSmallMouth AS OuncesBehind
+  FROM cteTotalWeight tw 
+  CROSS JOIN cteMaxWeight mw  
+), cteFinalResults AS (
+  SELECT a.angler,
+    concat(floor(l.MaxLargeMouthWeight/16), '-', mod(l.MaxLargeMouthWeight,16)) AS largemouth_weight,
+    concat(floor(s.MaxSmallMouthWeight/16), '-', mod(s.MaxSmallMouthWeight,16)) AS smallmouth_weight,
+    concat(floor(w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth/16), '-', mod(w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth,16)) AS total_weight,
+    concat(floor(b.OuncesBehind/16), '-', mod(b.OuncesBehind,16)) AS behind,
+    w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth AS total_weight_oz
+  FROM basstracker_anglers a
+  LEFT JOIN cteMaxLargeMouth l ON l.angler = a.angler
+  LEFT JOIN cteMaxSmallMouth s ON s.angler = a.angler
+  LEFT JOIN cteTotalWeight w ON w.angler = a.angler
+  LEFT JOIN cteOuncesBehind b ON b.angler = a.angler
+  WHERE 
+    w.TotalWeightOfAnglersMaxLargeAndMaxSmallMouth IS NOT null
+    and (a.angler = 'jason.vue' 
+    or a.angler = 'chewy.lee'
+    or a.angler = 'cubby.xiong'
+    or a.angler = 'daniel.zimmerschied'
+    or a.angler = 'sue.her'
+    or a.angler = 'kevin.rush'
+    or a.angler = 'douglas.zimmerschied'
+    or a.angler = 'chue.her')
+)
+SELECT
+  RANK() OVER(ORDER BY total_weight_oz desc),
+  angler,
+  largemouth_weight,
+  smallmouth_weight,
+  total_weight,
+  behind
+FROM cteFinalResults
+ORDER BY rank asc
